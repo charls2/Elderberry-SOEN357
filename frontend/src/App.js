b7507ff35575d51d4ebff959a9e1ad42d5ec9cb2
SSpaceApe <omar-tawil@outlook.com>
2024-11-24 17:08:28 -0500

    added browserrouter and dummy pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/Home">Home</Link></li>
+            <li><Link to="/About">About Us</Link></li>
+            {/* <li><a href="/contact">Contact</a></li> */}
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/contact" element={<Contact />} />
+          INSERT OTHER PAGE ROUTES HERE */}
+        </Routes>
+      </div>
+   
+   
   );
 }
 
